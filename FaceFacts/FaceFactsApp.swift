383d8ae113232c1acb714b73935757244f63a3a0
Brad Siegel <blsiege@gmail.com>
2024-02-27 13:09:32 -0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Brad Siegel on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
